af3c342921faddcd5c76aee7d961e835ca2e16fd
Doug <linderd@iinet.net.au>
2012-08-23 15:22:57 +0800

    Project cleanup

--- src/main/java/au/net/iinet/plugins/build/LeaderboardBuildPostAction.java
+++ src/main/java/au/net/iinet/plugins/build/LeaderboardBuildPostAction.java
@@ -29,23 +29,8 @@ import com.atlassian.bamboo.resultsummary.tests.TestClassResult;
 
 import static com.google.common.base.Preconditions.*;
 
-import com.atlassian.bamboo.build.BuildLoggerManager;
-import com.atlassian.bamboo.build.CustomBuildProcessorServer;
-import com.atlassian.bamboo.build.fileserver.BuildDirectoryManager;
-import com.atlassian.bamboo.plan.Plan;
-import com.atlassian.bamboo.process.EnvironmentVariableAccessor;
-import com.atlassian.bamboo.process.ProcessService;
-import com.atlassian.bamboo.v2.build.BaseConfigurablePlugin;
-import com.atlassian.bamboo.v2.build.BuildContext;
-import com.atlassian.bamboo.variable.CustomVariableContext;
-import com.atlassian.bamboo.ww2.actions.build.admin.create.BuildConfiguration;
-import org.apache.log4j.Logger;
 import com.atlassian.bamboo.commit.Commit;
 import com.google.common.collect.Multimap;
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion.User;
-
-import java.util.Map;
-import java.util.List;
 
 public class LeaderboardBuildPostAction implements PostJobAction {
 
@@ -55,30 +40,25 @@ public class LeaderboardBuildPostAction implements PostJobAction {
   
   public LeaderboardBuildPostAction(ActiveObjects ao) {
     this.ao = checkNotNull(ao);
-    users = new UserRepo(ao);
+    users = new UserRepo(this.ao);
   }
   
   @Override
   public void execute(StageExecution arg0, Job arg1, BuildResultsSummary arg2) {
-    System.err.println("------------ HELLO WORLD ------------------");
-    System.err.println("ao: " + ao);
-    System.err.println("Summary: " + arg2.getTestSummary());
     
     FilteredTestResults<TestClassResult> results = arg2.getFilteredTestResults();
     Multimap<TestClassResult, TestCaseResult> failed = results.getAllFailedTests();
     Multimap<TestClassResult, TestCaseResult> new_failed = results.getNewFailedTests();
-    Multimap<TestClassResult, TestCaseResult> exist_failed = results.getExistingFailedTests();
     Multimap<TestClassResult, TestCaseResult> fixed = results.getFixedTests();
     System.err.println("Failed tests: " + failed.size());
     
     int count_failed = failed.size();
     int count_new_failed = new_failed.size();
-    int count_exist_failed = exist_failed.size();
     int count_fixed = fixed.size();
     
-    /** HARD CODED LOGIC HAX */
-    
-    /** Copied shamelessly from jenkins CI game:
+    /** 
+       Copied shamelessly from jenkins CI game:
+       
        -10 points for breaking a build
         0 points for breaking a build that already was broken
         +1 points for doing a build with no failures (unstable builds gives no points)
@@ -88,30 +68,22 @@ public class LeaderboardBuildPostAction implements PostJobAction {
     
     /** Calculate the score for whoever just commited all that */
     int new_points = 0;
-    if (count_new_failed > 0) {
-      // Fucker, you broke the build. 
+    
+    // you broke the build. 
+    if (count_new_failed > 0) 
       new_points -= 10;
-      System.err.println("Build broke: -10");
-    }
     
     // You broke tests? Lose points
     new_points -= count_new_failed;
-    if (count_new_failed > 0) 
-      System.err.println("New fails: -" + count_new_failed);
     
     // You fixed some tests tho? Get points for that.
     new_points += count_fixed;
-    if (count_fixed > 0) 
-      System.err.println("Fixed tests: +" + count_fixed);
     
     // +1 for commit with no fails
-    if (count_failed == 0) {
+    if (count_failed == 0) 
       new_points += 1;
-      System.err.println("No fails: +1");
-    }
     
     for (Commit c : arg2.getCommits()) {
-      System.err.println("Commit " + c.getComment() + " by " + c.getAuthor().getLinkedUserName() + " // " + c.getAuthor().getLinkedUserName() + " - id " + c.getChangeSetId());
       String author_id = c.getAuthor().getName();
       String author_name = c.getAuthor().getName();
       
@@ -124,14 +96,7 @@ public class LeaderboardBuildPostAction implements PostJobAction {
       u.setPoints(total_points);
       
       users.save(u);
-      
-      System.err.println("Total update to user '" + author_id + "': " + new_points);
     }
-    System.err.println("------------ HELLO WORLD ------------------");
-  }
-  
-  private void trace(String msg) {
-    System.err.println(msg);
   }
 }
   
